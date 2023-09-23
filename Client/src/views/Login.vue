899fbb33c6eef3bcc2d2bfe80b615252c3217ad8
Alexander Ege <alexander@ege.com>
2023-09-23 16:01:07 -0700

    Styled login and register pages

--- Client/src/views/Login.vue
+++ Client/src/views/Login.vue
@@ -1,20 +1,19 @@
 <template>
-    <div>
-        
+    <div class="login-box">
+
         <!-- Profile Icon -->
-        <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="Empty Profile Picture">
+        <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Empty Profile Picture">
         
-        <form @submit.prevent="doLogin">
+        <form @submit.prevent="doLogin" class="login-form">
 
-        <!-- User input fields -->
-        <label for="username">Username</label>
-        <input type="text" v-model="username">
-
-        <label for="password">Password</label>
-        <input type="text" v-model="password">
-
-        <button>Login</button>
-
+            <label for="username">Username</label>
+            <input type="text" v-model="username">
+    
+            <label for="password">Password</label>
+            <input type="password" v-model="password">
+    
+            <button>Login</button>
+            <router-link to="/register">Create Account</router-link>
         </form>
     </div>
 </template>
@@ -40,3 +39,47 @@
         .catch((err) => console.log("error:", err))
     }
 </script>
+<style scoped>
+.login-box {
+    color: white;
+    width: 50%;
+    height: 50%;
+    margin: 2em auto;
+    padding: 2em;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 1em;
+}
+
+.login-form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 2em;
+}
+
+.login-form input[type=text], .login-form input[type=password] {
+    padding: 12px 20px;
+    margin: 8px 0;
+}
+
+.login-form button {
+    cursor: pointer;
+    height: 3em;
+    margin: 8px 0;
+    border: none;
+    background-color: #04AA6D;
+    color: white;
+}
+
+.login-form label, .login-form input {
+    width: 100%;
+}
+
+.login-box img {
+    width: 15em;
+    border-radius: 50%;
+}
+</style>
--- Client/src/views/Register.vue
+++ Client/src/views/Register.vue
@@ -1,18 +1,19 @@
 <template>
-    <div>
-        <img id="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" class="profile-img-card" />
+    <div class="register-box">
+        <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Empty Profile Picture">
 
-        <form @submit.prevent="onRegister">
+        <form @submit.prevent="onRegister" class="register-form">
             <label for="username">Username</label>
             <input type="text" v-model="username">
 
             <label for="email">Email</label>
             <input type="text" v-model="email">
 
-            <label for="password">password</label>
-            <input type="text" v-model="password">
+            <label for="password">Password</label>
+            <input type="password" v-model="password">
 
             <button>Register</button>
+            <router-link to="/login">Login</router-link>
         </form>
     </div>
 </template>
@@ -43,3 +44,47 @@ async function onRegister() {
 
 }
 </script>
+<style scoped>
+.register-box {
+    color: white;
+    width: 50%;
+    height: 50%;
+    margin: 2em auto;
+    padding: 2em;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 1em;
+}
+
+.register-form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 2em;
+}
+
+.register-form input[type=text], .register-form input[type=password] {
+    padding: 12px 20px;
+    margin: 8px 0;
+}
+
+.register-form button {
+    cursor: pointer;
+    height: 3em;
+    margin: 8px 0;
+    border: none;
+    background-color: #04AA6D;
+    color: white;
+}
+
+.register-form label, .register-form input {
+    width: 100%;
+}
+
+.register-box img {
+    width: 15em;
+    border-radius: 50%;
+}
+</style>
